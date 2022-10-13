f7c00f9b5fad01d6c0e9c5fffff5d04a4c056ceb
Aleksandra Konefał <a.konefal@madkom.pl>
2022-10-13 19:48:29 +0200

    zapis selektorów

---
+++ cypress/e2e/selectors.spec.cy.js
@@ -0,0 +1,19 @@
+describe('Lokatory', () => {
+  it('Lokatory', () => {
+    cy.visit("https://www.ebay.pl/");
+    cy.get("#gh-ac").type("sukienka");
+
+    cy.get("[name='_nkw']");
+
+    cy.get("[placeholder='Wyszukaj']");
+
+    cy.contains("Moda");
+
+    cy.get(".h1-cat-nav__js-tab").contains("Moda");
+  })
+})
+
+
+
+
+
